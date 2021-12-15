1d38aecf2ed2bd437ab54a70e81f49a6017aae19
SixtenNordegren <sixten.nordegren@gmail.com>
2021-12-15 14:35:50 +0100

    Data that actually works this time

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -1,34 +1,23 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-file_name = "~/water_electrolysis/data_analysis/data_5.txt"
-delta = 1
-min_lvl = 1
-data = np.loadtxt(file_name, delimiter=" ", dtype=float)
+from galvani import BioLogic as BL
+import pandas as pd
 
-def significance(margin, data_array):
-    ans = "No significant value"
-    for i in range(len((data_array))):
-        if i == 1:
-            pass
-        else:
-            diff = data_array[i] - data_array[i-1]
+mpr = BL.MPRfile('re_AgCl_we_Pd_ce_PT_el_15.mpr')
+df = pd.DataFrame(mpr.data)
+df.head()
 
-            if margin > diff:
-                ans = (i, data_array[i])
-                break
-            elif data[i] > min_lvl:
-                ans = (i, data_array[i])
-                break
-    return ans
+# file_name = "re_AgCl_we_Pd_ce_PT_el_15.mpr"
+# file_name = bl.MPRfile("re_AgCl_we_Pd_ce_PT_el_15.mpr")
+# data = pd.DataFrame(file_name)
+
 
 def main():
-    # significance(delta, data)
-    print(data[:10, :10])
+    print(df)
+    # plt.plot(df)
+    # plt.show()
 
 
 if __name__ == "__main__":
-    # data_plotable = data
-    # plt.plot(data_plotable)
-    # plt.show()
     main()
